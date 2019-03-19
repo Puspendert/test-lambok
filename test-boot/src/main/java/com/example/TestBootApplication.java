964a8499a5d4e42b89a2f356f379b00440068f1c
Puspender Tanwar <puspendertan@gmail.com>
2019-03-20 00:59:34 +0530

    added code to use the setter and getter methods

--- test-boot/src/main/java/com/example/TestBootApplication.java
+++ test-boot/src/main/java/com/example/TestBootApplication.java
@@ -1,13 +1,16 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestBootApplication.class, args);
+		
+		User user = new User();		
+		user.setName("hero");
+		System.out.println(user.getName());
+
 	}
 
 }
